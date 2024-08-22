ee947abc2b7f4bfd267a60075c0afd70112303f6
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-08-22 16:48:27 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post("/upload", async (c) => {
+  const body = await c.req.parseBody()
+  console.log(body.file)
+})
+
+export default app
